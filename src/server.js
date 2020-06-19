8d2e5217bac0ae0b678cadd7eee5782632eb149c
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-06-19 02:19:58 -0300

    Refatorando o código para melhor desempenho

--- src/server.js
+++ src/server.js
@@ -20,9 +20,8 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html");
 });
 
-server.get("/search-results", (req, res) => {
+server.get("/search", (req, res) => {
     return res.render("search-results.html");
 });
-
 
 server.listen(3000);
